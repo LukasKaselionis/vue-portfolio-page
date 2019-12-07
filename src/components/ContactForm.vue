809fa861112a7c3503c751b0faea0537d73dcac5
LukasKaselionis <l.kaselionis@gmail.com>
2019-12-07 17:06:07 +0200

    create "contact us" form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-form
+            ref="form"
+    >
+        <v-text-field
+                class=""
+                v-model="name"
+                :counter="10"
+                label="Name"
+                solo-inverted
+                required
+        ></v-text-field>
+
+        <v-text-field
+                class=""
+                v-model="email"
+                label="E-mail"
+                solo-inverted
+                required
+        ></v-text-field>
+
+        <v-textarea
+                solo-inverted
+        ></v-textarea>
+
+        <v-btn rounded color="black" dark>Send message</v-btn>
+
+    </v-form>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            valid: true,
+            name: '',
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+        }),
+
+        methods: {},
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,17 @@
 <template>
     <div id="contact" class="container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id incidunt quasi ratione veritatis. Aliquid
-            architecto deserunt hic inventore iure iusto magni quisquam sunt temporibus totam. Amet iste optio sequi
-            voluptatibus.
-        </p>
+        <ContactForm/>
     </div>
 </template>
 
 <script>
+    import ContactForm from "../components/ContactForm";
+
     export default {
-        name: "Contact"
+        name: "Contact",
+        components: {
+            ContactForm
+        }
     }
 </script>
 
