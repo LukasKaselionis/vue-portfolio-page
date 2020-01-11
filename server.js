f71246d680e9dc92a994c122849b59643ae6debd
LukasKaselionis <l.kaselionis@gmail.com>
2020-01-11 12:05:04 +0200

    fixed problems

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname + '/dist')));
 
 const port = process.env.PORT || 8080;
-app.listen(port, (req, res) => {
-    console.log(`server listening on port: ${port}`);
-});
+
+app.listen(port);
+
